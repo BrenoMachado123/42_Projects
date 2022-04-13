121a9a26cefddf75852d85907bd02410f8ef2fea
Breno Machado <bmachado@c1r6s6.42lisboa.com>
2022-04-13 20:59:23 +0100

    ...

--- ft_containers/inc/vector.hpp
+++ ft_containers/inc/vector.hpp
@@ -12,18 +12,18 @@ namespace ft {
 	class vector {
 		public:
 			//type_defintions
-			typedef T															value_type;
-			typedef Allocator													allocator_type;
-			typedef typename std::size_t										size_type;
-			typedef typename std::ptrdiff_t										difference_type;
-			typedef	typename allocator_type::reference							reference;
-			typedef typename allocator_type::const_reference					const_reference;
-	 		typedef	typename allocator_type::pointer							pointer;
-			typedef typename allocator_type::const_pointer						const_pointer;
-			typedef typename ft::random_access_iterator<pointer>				iterator;
-			typedef typename ft::random_access_iterator<const_pointer>			const_iterator;
-			typedef typename ft::reverse_iterator<pointer>						reverse_iterator;
-			typedef typename ft::reverse_iterator<const_pointer>				const_reverse_iterator;
+			typedef T	value_type;
+			typedef Allocator	allocator_type;
+			typedef typename std::size_t	size_type;
+			typedef typename std::ptrdiff_t	difference_type;
+			typedef	typename allocator_type::reference	reference;
+			typedef typename allocator_type::const_reference	const_reference;
+	 		typedef	typename allocator_type::pointer	pointer;
+			typedef typename allocator_type::const_pointer	const_pointer;
+			typedef typename ft::random_access_iterator<pointer>	iterator;
+			typedef typename ft::random_access_iterator<const_pointer>	const_iterator;
+			typedef typename ft::reverse_iterator<pointer>	reverse_iterator;
+			typedef typename ft::reverse_iterator<const_pointer>	const_reverse_iterator;
 
 
 			//construction
